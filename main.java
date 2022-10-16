d247289bae01e94cf8c7e2644a740931966bb2d7
bileam mangalla <99231083+bileam@users.noreply.github.com>
2022-10-16 20:13:57 +0700

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-import org.w3c.dom.html.HTMLParagraphElement;
-
 public class main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
